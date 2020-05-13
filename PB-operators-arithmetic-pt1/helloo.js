c6c82afc47cc2b39120a18e65599109f807f5509
putkara <natia.meskhi.natia@gmail.com>
2020-05-13 15:46:33 +0200

    finish

---
+++ PB-operators-arithmetic-pt1/helloo.js
@@ -0,0 +1,22 @@
+let x = 5,
+    y = 6;
+
+console.log(x * y);
+console.log(x - y);
+console.log(x / y);
+let z = 10;
+let resultOne = x * y / z;
+console.log(resultOne);
+let a = 15;
+let b = 9;
+console.log(a / b);
+
+let c = 20;
+let resultTwo = (a + b) * c; 
+console.log(resultTwo);
+console.log(++a);
+console.log(--b);
+let d = b - a;
+console.log(d);
+console.log(d + c);
+console.log(resultOne % resultTwo);
