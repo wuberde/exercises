1ab605712fb6341c6cea7d485a5a8e64d9824bf3
wuberde <60132580+wuberde@users.noreply.github.com>
2020-05-13 23:58:23 +0200

    Update index.js

--- 02-PB-operators-arithmetic-pt1/index.js
+++ 02-PB-operators-arithmetic-pt1/index.js
@@ -1,43 +1,43 @@
-// 0. 
+// 0.
 let x = 6;
-console.log(x, typeof (x));
+console.log(x, typeof x);
 let y = 7;
-console.log(y, typeof (y));
+console.log(y, typeof y);
 
-// 1. 
+// 1.
 console.log(x + y);
 
-// 2. 
+// 2.
 console.log(x - y);
 console.log(y - x);
 
-// 3. 
+// 3.
 console.log(x * y);
 
-// 4. 
+// 4.
 console.log(x / y);
 
 // 5.
 let z = 10;
-console.log(z, typeof (z));
+console.log(z, typeof z);
 let resultOne = (x * y) / z;
 console.log(resultOne);
 
-// 6. 
+// 6.
 let a = 15;
 let b = 9;
 console.log(a / b);
 
-// 7. 
+// 7.
 let c = 20;
-console.log(c, typeof (c));
+console.log(c, typeof c);
 let resultTwo = (a + b) * c;
 console.log(resultTwo);
 
 // 8.
 console.log(++a);
 
-// 9. 
+// 9.
 console.log(--b);
 
 // 10. my question: the answer should be 14 but why is the machine calculation result 12?
@@ -45,5 +45,5 @@ let d = b - a;
 console.log(d);
 console.log(d + c);
 
-// 11. 
+// 11.
 console.log(resultOne % resultTwo);
