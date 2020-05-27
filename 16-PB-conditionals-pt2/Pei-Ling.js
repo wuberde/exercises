7f27721870f6074ca57dda78b442d5fdcd8b8e0d
wuberde <wuberde@gmail.com>
2020-05-27 12:43:17 +0200

    Pei-Ling 16

--- 16-PB-conditionals-pt2/Pei-Ling.js
+++ 16-PB-conditionals-pt2/Pei-Ling.js
@@ -1,15 +1,3 @@
-/* # Programming Basics: Conditionals Part 2
-
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height). (mass in kg, i.e. 72.5, and height in metres, i.e. 1.65).
-
-1. Store Mark's and John's mass and height in variables.
-
-2. Calculate both their BMIs and store their BMIs in variables. 
-3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
-4. Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is"). 
-5. Create an *if* statement which prints the name and BMI of the person with the highest BMI. 
- */
-
 // 1. Store Mark's and John's mass and height in variables.
 let heightMark = 1.7;
 let massMark = 65;
@@ -41,4 +29,42 @@ if (bmiMark > bmiJohn) {
     console.log(`5: Mark has the highest BMI with ${bmiMark}`);
 } else {
     console.log(`5: John has the highest BMI with ${bmiJohn}`);
+}
+
+/* # Programming Basics: Conditionals Part 2
+
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height). (mass in kg, i.e. 72.5, and height in metres, i.e. 1.65).
+
+1. Store Mark's and John's mass and height in variables.
+2. Calculate both their BMIs and store their BMIs in variables. 
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is"). 
+5. Create an *if* statement which prints the name and BMI of the person with the highest BMI. 
+ */
+/* 
+Martina's solution:
+*/
+
+let markMass = 72.5;
+let markHeight = 1.65;
+let johnMass = 95;
+let johnHeight = 1.90;
+
+let bmiMark = markMass / (markHeight * markHeight);
+let bmiJohn = johnMass / johnHeight ** 2;
+
+console.log('bmi mark', bmiMark);
+console.log('bmi john', bmiJohn);
+
+let isMarksBMIhigher = bmiMark > bmiJohn; // boolean
+
+console.log(isMarksBMIhigher);
+
+console.log(`Is Marks BMI higher than Johns? 
+${isMarksBMIhigher}`);
+
+if (isMarksBMIhigher) {
+    console.log('Mark');
+} else {
+    console.log('John');
 }
