e58886fb888c472db76580e3b89816622fff31d4
wuberde <wuberde@gmail.com>
2020-05-27 11:43:52 +0200

    Pei-Ling 16

--- 16-PB-conditionals-pt2/Pei-Ling.js
+++ 16-PB-conditionals-pt2/Pei-Ling.js
@@ -29,11 +29,11 @@ let compareBmi;
 console.log(compareBmi = bmiMark > bmiJohn ? true : false);
 
 // 4. Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is"). 
-console.log(`Is Mark's BMI higher than John's? The answer is`, compareBmi);
+console.log(`Is Mark's BMI higher than John's? The answer is ${compareBmi}`);
 
 // 5. Create an *if* statement which prints the name and BMI of the person with the highest BMI. 
 if (bmiMark > bmiJohn) {
-    console.log(`5: Mark has the highest BMI with`, bmiMark)
+    console.log(`5: Mark has the highest BMI with ${bmiMark}`);
 } else {
-    console.log(`5: John has the highest BMI with`, bmiJohn);
-};
+    console.log(`5: John has the highest BMI with ${bmiJohn}`);
+}
