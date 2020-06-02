84f493a2de052580457441e0e8b3af659be7e64e
wuberde <wuberde@gmail.com>
2020-06-02 10:59:45 +0200

    Pei-Ling 20

---
+++ 20-PB-program-multiplechoice/Pei-Ling.js
@@ -0,0 +1,83 @@
+//# Switch if...
+
+// #### 1. Color Analyzer
+let myColor = `purple`;
+switch (myColor) {
+    case `red`:
+        console.log(`It's red.`);
+        break;
+    case `blue`:
+        console.log(`It's blue.`);
+        break;
+    case `green`:
+        console.log(`It's green.`);
+        break;
+    case `yellow`:
+        console.log(`It's yellow.`);
+        break;
+    default:
+        console.log(`It's not red, blue, green, or yellow. It's ${myColor}.`);
+}
+
+// #### 2. Grading
+let yourScore = `1`;
+switch (yourScore) {
+    case `1`:
+        console.log(`Very good!`);
+        break;
+    case `2`:
+        console.log(`Good!`);
+        break;
+    case `3`:
+        console.log(`Satisfactory.`);
+        break;
+    case `4`:
+        console.log(`Sufficient.`);
+        break;
+    case `5`:
+        console.log(`Insufficient.`);
+        break;
+    default:
+        console.log(`You did not take the exam.`);
+}
+
+// #### 3. Fruits
+let myFruit = `banana`;
+switch (myFruit) {
+    case `banana`:
+        console.log(`Bananas are 0.88 €/kg.`);
+        break;
+    case `orange`:
+        console.log(`Oranges are 1.99 €/kg.`);
+        break;
+    case `strawberry`:
+        console.log(`Strawberries are 1.49 €/500g.`);
+        break;
+    case `apple`:
+        console.log(`Apple are 1.49 €/kg.`);
+        break;
+}
+
+// #### 4. Percentage Complete.
+let percentageComplete = 50;
+switch (percentageComplete === 50) {
+    case percentageComplete < 30:
+        console.log(`Still a long way to go.`);
+        break;
+    case 30 <= percentageComplete <= 50:
+        console.log(`Slowly getting there.`);
+        break;
+    case 51 <= percentageComplete <= 80:
+        console.log(`You can do it!`);
+        break;
+    case 81 <= percentageComplete <= 99:
+        console.log(`This is the last push!`);
+    case percentageComplete === 100:
+        console.log(`You're there. Well done!`);
+}
+
+// 5. Differences
+//    When should you use a switch statement versus an if else statement. Comment your answer in your js file.
+/* Answer: You are comparing multiple possible conditions of an expression and the expression itself is non-trivial.
+You have multiple values that may require the same code.
+You have some values that will require essentially all of another value's execution, plus only a few statements. */
