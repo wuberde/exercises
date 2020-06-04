dfc163767fbc71e7886c1697ab0767b57a5e6a91
wuberde <wuberde@gmail.com>
2020-06-04 11:22:06 +0200

    Pei-Ling 23

---
+++ 23-PB-nested-loops/Pei-Ling.js
@@ -0,0 +1,14 @@
+// Make a Christmas tree which is upside down!
+
+let space = "";
+for (let i = 10; i >= 1; i--) {
+    // simply change the (let i = 1; i <= 10;) to (let i = 10; i >= 1;)
+    space = "";
+    for (let j = i; j < 10; j++) {
+        space += " ";
+    }
+    for (let k = 0; k < i * 2 - 1; k++) {
+        space += "*";
+    }
+    console.log(space);
+}
