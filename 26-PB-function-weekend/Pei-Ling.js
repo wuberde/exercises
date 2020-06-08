c91f8c6e5164f5c08a56d7398431afe22aab348d
wuberde <wuberde@gmail.com>
2020-06-08 14:14:22 +0200

    Pei-Ling 26

---
+++ 26-PB-function-weekend/Pei-Ling.js
@@ -0,0 +1,14 @@
+`use strict`;
+const herBday = new Date(`2020-07-10`);
+// const dayOfWeek = herBday.getDay();
+// console.log(dayOfWeek);
+
+weekOrWeekend = (herBday) => {
+    
+    if (herBday.getDay() === 6 || herBday.getDay() === 0) {
+        console.log(`Her birthday this year will be on the weekend.`);
+    } else {
+        console.log(`Her birthday this year will be a weekday.`);
+    }
+}
+weekOrWeekend(herBday, weekOrWeekend);
