ed3e5eda1615da6e14eb4c1a4360e55f9a40da21
wuberde <wuberde@gmail.com>
2020-06-09 08:35:52 +0200

    Pei-Ling 27 not finished

--- 27-PB-more-on-dates/Pei-Ling.js
+++ 27-PB-more-on-dates/Pei-Ling.js
@@ -4,4 +4,35 @@
 
 `use strict`;
 
-const today = new Date(`2020-07-10`);
+const today = new Date();
+const toDate= today.getDate();
+console.log(toDate);
+
+const remainingDaysOfMonth = (days) => {
+    days = 30;
+    return days - toDate;
+}
+remainingDaysOfMonth();
+
+/* 
+<script> 
+var up = document.getElementById('GFG_UP'); 
+  
+up.innerHTML = "Click on button to get the" 
+    + " number of days in specified month"; 
+      
+var down = document.getElementById('GFG_DOWN');  
+  
+function daysInMonth (month, year) { 
+    return new Date(year, month, 0).getDate(); 
+} 
+  
+function GFG_Fun() { 
+    var date = new Date(); 
+    var month = 2; 
+    var year = 2020; 
+    down.innerHTML = "Number of days in " + month 
+                 + "nd month of the year " + year 
+                 +" is "+ daysInMonth(month, year); 
+} 
+</script>   */
