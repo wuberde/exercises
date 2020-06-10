c71c2e2ae22b5c9041fd769cdd9fa18d5f14ba39
wuberde <wuberde@gmail.com>
2020-06-10 10:21:25 +0200

    Pei-Ling 29

--- 29-PB-functions/Pei-Ling.js
+++ 29-PB-functions/Pei-Ling.js
@@ -11,10 +11,10 @@ const addUp = (num) => {
 console.log(`Task 1:`, addUp(13));
 
 // **2. Cubed.**
-const sumOfCubes = (a, b, c) => {
-    return (a **= 3) + (b **= 3) + (c **= 3);
-};
-console.log(`Task 2:`, sumOfCubes(1, 5, 9));
+const sumOfCubes = (a = 0, b = 0, c = 0) => a ** 3 + b ** 3 + c ** 3;
+console.log(`Task 2, ex. 1:`, sumOfCubes(1, 5, 9));
+console.log(`Task 2, ex. 2:`, sumOfCubes(2));
+console.log(`Task 2, ex. 3:`, sumOfCubes());
 
 // **3. String Check.**
 const dictionary = (a, b) => {
@@ -23,7 +23,7 @@ const dictionary = (a, b) => {
     } else {
         return false;
     }
-}
+};
 console.log(`Task 3, ex. 1:`, dictionary("bu", "button"));
 console.log(`Task 3, ex. 2:`, dictionary("beau", "pastry"));
 
@@ -34,38 +34,44 @@ const lessThanOrEqualToZero = (a) => {
     } else {
         return false;
     }
-}
+};
 console.log(`Task 4, ex. 1:`, lessThanOrEqualToZero(3));
 console.log(`Task 4, ex. 2:`, lessThanOrEqualToZero(0));
 console.log(`Task 4, ex. 3:`, lessThanOrEqualToZero(-4));
+console.log(`Task 4, ex. 4:`, lessThanOrEqualToZero(10));
 
-// **5. Count Occurrences.** 
+// **5. Count Occurrences.**
 
 const countOccurrences = (longestEnglishWord, x) => {
     return longestEnglishWord.split(x).length - 1;
-}
-console.log(`Task 5: How many times does the letter o appear in the longest English word "pneumonoultramicroscopicsilicovolcanoconiosis"?`, countOccurrences("pneumonoultramicroscopicsilicovolcanoconiosis", "o"));
+};
+console.log(
+    `Task 5: How many times does the letter o appear in the longest English word "pneumonoultramicroscopicsilicovolcanoconiosis"?`,
+    countOccurrences("pneumonoultramicroscopicsilicovolcanoconiosis", "o")
+);
 
 /* let longestGermanWord = `Donau­dampf­schiffahrts­elektrizitäten­haupt­betriebs­werk­bau­unter­beamten­gesellschaft`;
 console.log(`How many characters?`, longestGermanWord.length);  */
 
 // **6. X To The Power of X.**
 const calculateBaseToExponent = (base, exponent) => {
-    return base **= exponent;
-}
+    return (base ** exponent);
+};
 console.log(`Task 6:`, calculateBaseToExponent(2, 3));
 
 // **7. Dog Years.**
 const dogAge = (a) => {
-
     return a * 7;
-}
-console.log(`Task 7: According to Guinness World Records, the dog named Bluey lived 29 years, which converted to the human age of`, (Math.round(dogAge(29.416666667))), `years old.`);
-
-// Below isn't finished yet
+};
+console.log(
+    `Task 7: According to Guinness World Records, the dog named Bluey lived 29 years, which converted to the human age of`,
+    Math.round(dogAge(29.416666667)),
+    `years old.`
+);
 
 // **8. A Lifetime Supply...**
 
 // **9. Where's Waldo?**
+// const isWaldoHere=()=>
 
 // **10. Pie.** 
