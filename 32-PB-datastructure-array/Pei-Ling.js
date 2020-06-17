6817152eb10eedffe98f545b47e4c204289d5ff5
wuberde <wuberde@gmail.com>
2020-06-17 11:33:11 +0200

    Pei-Ling 32

---
+++ 32-PB-datastructure-array/Pei-Ling.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const myArray = [1, `cat`, 2, `hoomans`, true];
+console.log(`Task 1: `, myArray);
+
+let thirdElement = myArray[2];
+console.log(`Task 2: The third element is `, thirdElement);
+
+myArray[3] = `fourth element`;
+console.log(myArray[3]);
