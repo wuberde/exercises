bd7bef1834d7a0edf8087e8b5c84ebeb522ed4ce
wuberde <wuberde@gmail.com>
2020-06-17 11:34:14 +0200

    Pei-Ling 32

--- 32-PB-datastructure-array/Pei-Ling.js
+++ 32-PB-datastructure-array/Pei-Ling.js
@@ -7,4 +7,4 @@ let thirdElement = myArray[2];
 console.log(`Task 2: The third element is `, thirdElement);
 
 myArray[3] = `fourth element`;
-console.log(myArray[3]);
+console.log(`Task 3: `, myArray[3]);
