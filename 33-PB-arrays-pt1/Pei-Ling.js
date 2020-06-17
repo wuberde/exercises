8946597bdab9a0c67deffda3be26b08197c54a16
wuberde <wuberde@gmail.com>
2020-06-17 15:13:36 +0200

    Pei-Ling 33

---
+++ 33-PB-arrays-pt1/Pei-Ling.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// 1.
+let euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
+console.log(`Task 1-1: `, euroCities);
+let euroCities2 = euroCities[1];
+console.log(`Task 1-2: `, euroCities2);
+
+// 2.
+console.log(`Task 2: `);
+euroCities[0] = `Berlin`;
+
+// 3.
+console.log(`Task 3: `, euroCities);
+
+// 4.
+console.log(`Task 4: `, euroCities.pop());
+console.log(euroCities);
+
+// 5.
+console.log(`Task 5: `, (euroCities[euroCities.length] = "Budapest"));
+console.log(euroCities);
+
+// 6.
+console.log(`Task 6: `, euroCities.splice(1, 2));
+console.log(euroCities);
+
+// 7.
+let asianCities = ["Taipei", "Tokyo", "Mumbai", "Hanoi", "Seoul"];
+console.log(`Task 7: `, asianCities);
+
+// 8.
+let asianCities1 = asianCities.slice(1, 4);
+console.log(`Task 8: `, asianCities1);
+
+// 9.
+let worldCities = [`${euroCities}, ${asianCities}`];
+console.log(`Task 9: `, worldCities);
+// console.log(typeof(worldCities));
+
+// 10.
+let worldCitiesRev = worldCities.reverse();
+console.log(`Task 10: reverse doesn't work...`, worldCitiesRev);
+
+// 11.
+worldCities[2] = `Toronto`;
+console.log(`Task 11: `, worldCities);
