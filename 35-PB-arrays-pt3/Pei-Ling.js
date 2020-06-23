17a7ffefdaec2169d1f80b666d119b7e1ecdb27a
wuberde <wuberde@gmail.com>
2020-06-23 10:27:31 +0200

    Pei-Ling 35

--- 35-PB-arrays-pt3/Pei-Ling.js
+++ 35-PB-arrays-pt3/Pei-Ling.js
@@ -36,7 +36,7 @@ console.log(unique([3, 3, 3, 7, 3, 3]));
 console.log(unique([0, 0, 0.77, 0, 0]));
 console.log(unique([0, 1, 1, 1, 1, 1, 1, 1]));
 
-// 5. (solution found online)
+// 5. 
 const wordRank = (s) => {
     let as = s.split(' ').map(s => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
     return s.split(' ')[as.indexOf(Math.max(...as))];
