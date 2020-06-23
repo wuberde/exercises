bbe22865b9148d4a9cb4613dae014f7d5f7e193f
wuberde <wuberde@gmail.com>
2020-06-23 10:26:39 +0200

    Pei-Ling 35

--- 35-PB-arrays-pt3/Pei-Ling.js
+++ 35-PB-arrays-pt3/Pei-Ling.js
@@ -35,3 +35,23 @@ function unique(array) {
 console.log(unique([3, 3, 3, 7, 3, 3]));
 console.log(unique([0, 0, 0.77, 0, 0]));
 console.log(unique([0, 1, 1, 1, 1, 1, 1, 1]));
+
+// 5. (solution found online)
+const wordRank = (s) => {
+    let as = s.split(' ').map(s => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
+    return s.split(' ')[as.indexOf(Math.max(...as))];
+}
+console.log(wordRank("The quick brown fox."));
+console.log(wordRank("Nancy is very pretty.")); //why not "pretty"?
+console.log(wordRank("Check back tomorrow, man!"));
+console.log(wordRank("Today is Wednesday."));
+
+// 6. 
+const hackerSpeak = (orig, replacements = {'a': '4', 'e': '3', 'i': '1', 'o': '0'}) => (
+    Object
+    .entries(replacements)
+    .reduce((acc, el) => acc.replace(el[0], el[1]), orig)
+) 
+console.log(hackerSpeak("javascript is cool")); // ➞"j4v45cr1pt 15 c00l"
+console.log(hackerSpeak("programming is fun")); // ➞"pr0gr4mm1ng 15 fun"
+console.log(hackerSpeak("become a coder")); // ➞"b3c0m3 4 c0d3r"
