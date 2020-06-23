2999d7d92eee05d8c5b6c35f0fedfc9075f2a3a9
wuberde <wuberde@gmail.com>
2020-06-23 13:33:06 +0200

    Pei-Ling 36

--- 36-PB-arrays-loops/Pei-Ling.js
+++ 36-PB-arrays-loops/Pei-Ling.js
@@ -2,28 +2,35 @@
 
 // 1. sumOfNumbers
 const sumOfNumbers = (nums) => {
-    return nums[0] + nums[1] + nums[2];
-}
-console.log(`The sum of ${nums="[3, 2, -1]"} is ${sumOfNumbers([3, 2, -1])}.`);
+  return nums[0] + nums[1] + nums[2];
+};
+console.log(
+  `The sum of ${(nums = "[3, 2, -1]")} is ${sumOfNumbers([3, 2, -1])}.`
+);
 
 // 2. Hello Frien
-const helloFrien = (names) => {
-
-    for (i = 0; i <= names.length; i++) {
-        return names+[i];
-    }
+const names = [`Maria`, `Mike`, `Joe`, `Jane`];
+for (let i = 0; i < names.length; i++) {
+  console.log(`Hello`, names[i] + `!`);
 }
-console.log(`Hello ${helloFrien([`Maria`, `Mike`, `Joe`, `Jane`])}`);
 
+// 3. City Names (Andrea presented)
+let cityNames = [`Berlin`, `Paris`, `Rome`, `Prague`];
+let string = ` `;
+for (let i = 0; i < cityNames.length; i++) {
+  string += cityNames[i] + `, `;
+}
+string = string.substring(0, string.length - 2);
+console.log(string);
 
 // 5. Capitalize. Create a program that capitalises the first letter of each element in an array of names.
 
 const capitalizeName = (names) => {
-    for (let i = 0; i < names.length; i++) {
-        names[i] = names[i][0].toUpperCase() + names[i].slice(1);
-    }
-    return names;
-}
+  for (let i = 0; i < names.length; i++) {
+    names[i] = names[i][0].toUpperCase() + names[i].slice(1);
+  }
+  return names;
+};
 console.log(capitalizeName(["matt", "sara", "lara"])); // ➞ ["Matt", "Sara", "Lara"]
 console.log(capitalizeName(["samuel", "MARIA", "luke", "mary"])); // ➞ ["Samuel", "Maria", "Luke", "Mary"]
 console.log(capitalizeName(["Cynthia", "Karen", "Jane", "Carrie"])); // ➞["Cynthia", "Karen", "Jane", "Carrie"]
