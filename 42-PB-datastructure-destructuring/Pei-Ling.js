6f5bdded10b1e3ed57fac48b9950375e1a2f4eaa
wuberde <wuberde@gmail.com>
2020-06-29 15:44:16 +0200

    Pei-Ling 42

---
+++ 42-PB-datastructure-destructuring/Pei-Ling.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// 1.
+let fruit, vegetable, food;
+[fruit, vegetable] = ["banana", "cucumber"];
+[fruit, vegetable, ...food] = ["banana", "cucumber", "bread", "cakes", "pizza"];
+console.log(food);
+
+// 2. 
+let halloweenCostume = {
+    fran: "witch",
+    peter: "vampire",
+    bob: "frankenstein"
+};
+// console.log(halloweenCostume);
+let fran, peter, bob;
+({
+    fran,
+    peter,
+    bob
+} = halloweenCostume);
+console.log(fran);
+console.log(peter);
+console.log(bob);
+
+// 3.  Parameters: Object Destructuring 
+/* Create an object with band/musician details e.g. name of the band/musician, greatest hit, nationality, genre etc.
+Then, create a function to display all of the band/musician's data.
+Example
+`Regina Spektor is a American-Russian singer/songwriter and pianist. The musician sings indie-pop and their greatest hit is "Us"`. */
+// let obj={band: `AAA`, musician: `Adam`, hit: `Heaven`, 
